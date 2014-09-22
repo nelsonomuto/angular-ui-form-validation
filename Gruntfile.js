6eab7da78fddb7e89253ce6cef45334c0adddb7e
nelsonomuto <nelsonomuto@gmail.com>
2014-09-22 16:22:27 -0400

    creating cssmin dist target. fixes #32

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-debug');
 
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
@@ -351,16 +352,15 @@ module.exports = function (grunt) {
         // By default, your `index.html`'s <!-- Usemin block --> will take care of
         // minification. These next options are pre-configured if you do not wish
         // to use the Usemin blocks.
-        // cssmin: {
-        //   dist: {
-        //     files: {
-        //       '<%= yeoman.dist %>/styles/main.css': [
-        //         '.tmp/styles/{,*/}*.css',
-        //         '<%= yeoman.app %>/styles/{,*/}*.css'
-        //       ]
-        //     }
-        //   }
-        // },
+        cssmin: {
+           dist: {
+             files: {
+               '<%= yeoman.dist %>/styles/main.css': [
+                 '.tmp/styles/{,*/}*.css'
+               ]
+             }
+           }
+        },
         // uglify: {
         //   dist: {
         //     files: {
@@ -468,7 +468,7 @@ module.exports = function (grunt) {
         'ngAnnotate',
         'copy:dist',
         'cdnify',
-        'cssmin',
+        'cssmin:dist',
         'uglify',
         'rev',
         'usemin'
