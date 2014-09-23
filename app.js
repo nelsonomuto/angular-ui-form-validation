484a7d0b3693d1612a950910da343daf622e3c9f
Nelson Omuto <nelsonomuto@gmail.com>
2014-09-23 15:28:30 -0400

    updated gh pages after transfer to organization

---
+++ app.js
@@ -0,0 +1,7 @@
+var connect = require('connect')
+var serveStatic = require('serve-static')
+
+var app = connect();
+
+app.use(serveStatic('.', {'index': ['index.html', 'index.htm']}))
+app.listen(9000);
