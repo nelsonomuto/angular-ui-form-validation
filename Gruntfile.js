d91b99c5810c1c0e7c35e9f99d0b68fa8c8429a5
nelsonomuto <nelsonomuto@gmail.com>
2014-09-25 16:01:44 -0400

    fixes #36 how to handle form submission, will require an update to gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,10 +66,10 @@ module.exports = function (grunt) {
         // The actual grunt server settings
         connect: {
             options: {
-                port: 9000,
+                port: 9010,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost',
-                livereload: 35729
+                livereload: 35739
             },
             livereload: {
                 options: {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -38,7 +38,40 @@ angular.module('angularUiFormValidationApp')
         state: '',
         isAdmin: null,
         birthdate: new Date(),
-        hasKids: null
+        hasKids: null,
+        save: function () {
+            /**
+             * Trigger the runCustomValidations to ensure the form is evaluated before submission
+             */
+            $scope.$broadcast('runCustomValidations');
+
+            /**
+             * The customValidationComplete event will fire for each evaluated property of user
+             *
+             * @param {Object} data = {
+             *       isValid: {Boolean},
+             *       validation: {string} $element.attr('id') + ' ' + formatterArgs.customValidationAttribute + status,
+             *       model: {Object},
+             *       controller: {Object} ngModelController,
+             *       element: {Object} $element //DOM element
+             *   };
+             */
+            $scope.$on('customValidationComplete', function (data) {
+                //console.log(data);
+                //You may perform any business logic here based on the element validation results
+            });
+
+            /*
+             * The custom validations will display errors after evaluation performed by runCustomValidations is triggered above on line 46.
+             * This will also ensure that the $scope.demoForm.$valid is updated based upon aforementioned evaluation.
+             */
+            if($scope.demoForm.$valid) {
+                alert('Congratulations, the form is valid and ready to be submitted for further processing!');
+                //Submit for further processing
+            } else {
+                //do nothing or whatever business logic you require
+            }
+        }
       },
       states: ['', 'validState', 'invalidState1', 'invalidState2'],
       cities: ['', 'validCity', 'invalidCity1', 'invalidCity2'],
