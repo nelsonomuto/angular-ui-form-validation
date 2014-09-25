1bff1959cd06716fc052e0d108d5b12b63cd80c9
nelsonomuto <nelsonomuto@gmail.com>
2014-09-25 14:58:07 -0400

    fixes #35 and updating ignore to not add gh-pages test template to master

---
+++ index.js
@@ -0,0 +1,4 @@
+require("./dist/angular-ui-form-validation");
+
+module.exports = angular.module('directives.customvalidation.customValidationTypes');
+//module.exports = angular.module('angular.ui.form.validation');
