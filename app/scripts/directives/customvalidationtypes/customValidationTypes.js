0365f05b58509a1c8a114f7b137c64ee88e01b8b
Nelson Omuto <nelsonomuto@gmail.com>
2014-12-21 15:01:05 -0500

    initial

--- app/scripts/directives/customvalidationtypes/customValidationTypes.js
+++ app/scripts/directives/customvalidationtypes/customValidationTypes.js
@@ -3,150 +3,175 @@
         'directives.customvalidation.customValidations'
     ]);
 
-    getValidationAttributeValue = angular_ui_form_validations.getValidationAttributeValue;
+    extendCustomValidations.provider('myValidations', function () {
 
-    angular.forEach([
-         {
-            customValidationAttribute: 'validationFieldRequired',
-            errorMessage: 'This is a required field',
-            validator: function (errorMessageElement, val){
-                return (/\S/).test(val);
-            }
-         },
-         {
-            customValidationAttribute: 'validationConfirmPassword',
-            errorMessage: 'Passwords do not match.',
-            validator: function (errorMessageElement, val, attr, element, model, modelCtrl) {
-                var password = model.password || '';
-                return password.replace(/\s+$/, '') === element.val().replace(/\s+$/, '');
-            }
-         },
-         {
-            customValidationAttribute: 'validationEmail',
-            errorMessage: 'Please enter a valid email',
-            validator: function (errorMessageElement, val){
-                return (/^.*@.*\..*[a-z]$/i).test(val);
-            }
-         },
-         {
-            customValidationAttribute: 'validationNoSpace',
-            errorMessage: 'Cannot contain any spaces',
-            validateWhileEntering: true,
-            validator: function (errorMessageElement, val){
-                return val !== '' && (/^[^\s]+$/).test(val);
-            }
-         },
-         {
-            customValidationAttribute: 'validationMinLength',
-            errorMessage: function (attr) { return 'Minimum of ' + getValidationAttributeValue(attr) + ' characters'; },
-            validator: function (errorMessageElement, val, attr) {
-                return val.length >= parseInt(getValidationAttributeValue(attr), 10);
-            }
+      getValidationAttributeValue = angular_ui_form_validations.getValidationAttributeValue;
+      var outOfBoxValidations = [
+        {
+          customValidationAttribute: 'validationFieldRequired',
+          errorMessage: 'This is a required field',
+          validator: function (errorMessageElement, val){
+            return (/\S/).test(val);
+          }
         },
         {
-            customValidationAttribute: 'validationMaxLength',
-            errorMessage: '',
-            validateWhileEntering: true,
-            validator: function (errorMessageElement, val, attr, $element, model, ngModelController, $scope, rawAttr) {
-                var customMessage = getValidationAttributeValue(rawAttr, 'message', true);
-                if (val.length <= parseInt(getValidationAttributeValue(rawAttr), 10)) {
-                    return true;
-                } else {
-                    errorMessageElement.html(customMessage || 'Maximum of ' + getValidationAttributeValue(rawAttr) + ' characters');
-                    return false;
-                }
-            }
+          customValidationAttribute: 'validationConfirmPassword',
+          errorMessage: 'Passwords do not match.',
+          validator: function (errorMessageElement, val, attr, element, model, modelCtrl) {
+            var password = model.password || '';
+            return password.replace(/\s+$/, '') === element.val().replace(/\s+$/, '');
+          }
         },
         {
-            customValidationAttribute: 'validationOnlyAlphabets',
-            errorMessage: 'Valid characters are: A-Z, a-z',
-            validateWhileEntering: true,
-            validator: function (errorMessageElement, val){
-                return (/^[a-z]*$/i).test(val);
-            }
+          customValidationAttribute: 'validationEmail',
+          errorMessage: 'Please enter a valid email',
+          validator: function (errorMessageElement, val){
+            return (/^.*@.*\..*[a-z]$/i).test(val);
+          }
         },
         {
-            customValidationAttribute: 'validationOneUpperCaseLetter',
-            errorMessage: 'Must contain at least one uppercase letter',
-            validator: function (errorMessageElement, val){
-                return (/^(?=.*[A-Z]).+$/).test(val);
-            }
+          customValidationAttribute: 'validationNoSpace',
+          errorMessage: 'Cannot contain any spaces',
+          validateWhileEntering: true,
+          validator: function (errorMessageElement, val){
+            return val !== '' && (/^[^\s]+$/).test(val);
+          }
         },
         {
-            customValidationAttribute: 'validationOneLowerCaseLetter',
-            errorMessage: 'Must contain at least one lowercase letter',
-            validator: function (errorMessageElement, val){
-                return (/^(?=.*[a-z]).+$/).test(val);
-            }
+          customValidationAttribute: 'validationMinLength',
+          errorMessage: function (attr) { return 'Minimum of ' + getValidationAttributeValue(attr) + ' characters'; },
+          validator: function (errorMessageElement, val, attr) {
+            return val.length >= parseInt(getValidationAttributeValue(attr), 10);
+          }
         },
         {
-            customValidationAttribute: 'validationOneNumber',
-            errorMessage: 'Must contain at least one number',
-            validator: function (errorMessageElement, val){
-                return (/^(?=.*[0-9]).+$/).test(val);
+          customValidationAttribute: 'validationMaxLength',
+          errorMessage: '',
+          validateWhileEntering: true,
+          validator: function (errorMessageElement, val, attr, $element, model, ngModelController, $scope, rawAttr) {
+            var customMessage = getValidationAttributeValue(rawAttr, 'message', true);
+            if (val.length <= parseInt(getValidationAttributeValue(rawAttr), 10)) {
+              return true;
+            } else {
+              errorMessageElement.html(customMessage || 'Maximum of ' + getValidationAttributeValue(rawAttr) + ' characters');
+              return false;
             }
+          }
         },
         {
-            customValidationAttribute: 'validationOneAlphabet',
-            errorMessage: 'Must contain at least one alphabet',
-            validator: function (errorMessageElement, val) {
-                return (/^(?=.*[a-z]).+$/i).test(val);
-            }
+          customValidationAttribute: 'validationOnlyAlphabets',
+          errorMessage: 'Valid characters are: A-Z, a-z',
+          validateWhileEntering: true,
+          validator: function (errorMessageElement, val){
+            return (/^[a-z]*$/i).test(val);
+          }
         },
         {
-            customValidationAttribute: 'validationNoSpecialChars',
-            validateWhileEntering: true,
-            errorMessage: 'Valid characters are: A-Z, a-z, 0-9',
-            validator: function (errorMessageElement, val){
-                return (/^[a-z0-9_\-\s]*$/i).test(val);
-            }
+          customValidationAttribute: 'validationOneUpperCaseLetter',
+          errorMessage: 'Must contain at least one uppercase letter',
+          validator: function (errorMessageElement, val){
+            return (/^(?=.*[A-Z]).+$/).test(val);
+          }
         },
         {
-            customValidationAttribute: 'validationDateBeforeToday',
-            errorMessage: 'Must be prior to today',
-            validator: function (errorMessageElement, val){
-                var now, dateValue;
-                now = new Date();
-                dateValue = new Date(val);
-                dateValue.setDate(dateValue.getDate() + 1);
-                return dateValue < now;
-            }
+          customValidationAttribute: 'validationOneLowerCaseLetter',
+          errorMessage: 'Must contain at least one lowercase letter',
+          validator: function (errorMessageElement, val){
+            return (/^(?=.*[a-z]).+$/).test(val);
+          }
         },
         {
-            customValidationAttribute: 'validationDateBefore',
-            errorMessage: function (attr) { return 'Must be before ' + getValidationAttributeValue(attr); },
-            validator: function (errorMessageElement, val, beforeDate){
-                var dateValue = new Date(val);
-                dateValue.setDate(dateValue.getDate() + 1);
-                return dateValue < new Date(beforeDate);
-            }
+          customValidationAttribute: 'validationOneNumber',
+          errorMessage: 'Must contain at least one number',
+          validator: function (errorMessageElement, val){
+            return (/^(?=.*[0-9]).+$/).test(val);
+          }
         },
         {
-            customValidationAttribute: 'validationDateAfter',
-            errorMessage: function (attr) { return 'Must be after ' + getValidationAttributeValue(attr); },
-            validator: function (errorMessageElement, val, afterDate){
-                var dateValue = new Date(val);
-                dateValue.setDate(dateValue.getDate() + 1);
-                dateValue.setHours(0);
-                return dateValue > new Date(afterDate);
-            }
+          customValidationAttribute: 'validationOneAlphabet',
+          errorMessage: 'Must contain at least one alphabet',
+          validator: function (errorMessageElement, val) {
+            return (/^(?=.*[a-z]).+$/i).test(val);
+          }
+        },
+        {
+          customValidationAttribute: 'validationNoSpecialChars',
+          validateWhileEntering: true,
+          errorMessage: 'Valid characters are: A-Z, a-z, 0-9',
+          validator: function (errorMessageElement, val){
+            return (/^[a-z0-9_\-\s]*$/i).test(val);
+          }
+        },
+        {
+          customValidationAttribute: 'validationDateBeforeToday',
+          errorMessage: 'Must be prior to today',
+          validator: function (errorMessageElement, val){
+            var now, dateValue;
+            now = new Date();
+            dateValue = new Date(val);
+            dateValue.setDate(dateValue.getDate() + 1);
+            return dateValue < now;
+          }
+        },
+        {
+          customValidationAttribute: 'validationDateBefore',
+          errorMessage: function (attr) { return 'Must be before ' + getValidationAttributeValue(attr); },
+          validator: function (errorMessageElement, val, beforeDate){
+            var dateValue = new Date(val);
+            dateValue.setDate(dateValue.getDate() + 1);
+            return dateValue < new Date(beforeDate);
+          }
+        },
+        {
+          customValidationAttribute: 'validationDateAfter',
+          errorMessage: function (attr) { return 'Must be after ' + getValidationAttributeValue(attr); },
+          validator: function (errorMessageElement, val, afterDate){
+            var dateValue = new Date(val);
+            dateValue.setDate(dateValue.getDate() + 1);
+            dateValue.setHours(0);
+            return dateValue > new Date(afterDate);
+          }
         }
-    ],
+      ];
 
-    function(customValidation) {
-        extendCustomValidations.directive('input', function (customValidationUtil) {
+      var userCustomValidations = [];
+
+      this.addCustomValidations = function (validations) {
+        userCustomValidations = userCustomValidations.concat(validations);
+      };
+
+      var Validations = function () {
+        this.fetch = function () {
+          return userCustomValidations.concat(outOfBoxValidations);
+        };
+      };
+
+      this.$get = function () {
+        return new Validations();
+      };
+    });
+
+    extendCustomValidations.run(function(myValidations){
+      angular.forEach(myValidations.fetch(),
+
+        function(customValidation) {
+          debugger;
+          extendCustomValidations.directive('input', function (customValidationUtil) {
             return {
-                require: '?ngModel',
-                restrict: 'E',
-                link: customValidationUtil.createValidationLink(customValidation)
+              require: '?ngModel',
+              restrict: 'E',
+              link: customValidationUtil.createValidationLink(customValidation)
             };
-        });
-        extendCustomValidations.directive('textarea', function (customValidationUtil) {
+          });
+          extendCustomValidations.directive('textarea', function (customValidationUtil) {
             return {
-                require: '?ngModel',
-                restrict: 'E',
-                link: customValidationUtil.createValidationLink(customValidation)
+              require: '?ngModel',
+              restrict: 'E',
+              link: customValidationUtil.createValidationLink(customValidation)
             };
+          });
         });
     });
+
+
 })();
