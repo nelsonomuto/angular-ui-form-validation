e07839e2ab502d7e92dc534f49695d7ed3a46a72
Nelson Omuto <nelsonomuto@gmail.com>
2015-11-17 11:32:57 -0500

    ensuring that refactored folder paths are resolved for services and directives

--- karma.conf.js
+++ karma.conf.js
@@ -1,57 +1,86 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function(config) {
-  config.set({
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+module.exports = function (config) {
+    config.set({
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+        //reporters: ['html'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'app/bower_components/lazy.js/lazy.js',
-      'app/bower_components/jquery/jquery.js',
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-cookies/angular-cookies.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-template-retriever/dist/angular-template-retriever.js',
-      'app/scripts/**/*.js',
-      'test/**/*.js',
-    ],
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-    // list of files / patterns to exclude
-    exclude: [],
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
 
-    // web server port
-    port: 8082,
+        // list of files / patterns to load in the browser
+        files: [
+            'app/bower_components/lazy.js/lazy.js',
+            'app/bower_components/jquery/jquery.js',
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-resource/angular-resource.js',
+            'app/bower_components/angular-cookies/angular-cookies.js',
+            'app/bower_components/angular-sanitize/angular-sanitize.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-template-retriever/dist/angular-template-retriever.js',
+            'app/scripts/**/*.js',
+            'test/**/*.js',
+        ],
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // list of files / patterns to exclude
+        exclude: [],
+
+        // web server port
+        port: 8082,
+
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+        loggers: [
+            {
+                "type": "clustered",
+                "appenders": [
+                    {
+                        "type": "dateFile",
+                        "filename": "log/access.log",
+                        "pattern": "-yyyy-MM-dd",
+                        "category": "http"
+                    },
+                    {
+                        "type": "file",
+                        "filename": "log/app.log",
+                        "maxLogSize": 10485760,
+                        "numBackups": 3
+                    },
+                    {
+                        "type": "logLevelFilter",
+                        "level": "ERROR",
+                        "appender": {
+                            "type": "file",
+                            "filename": "log/errors.log"
+                        }
+                    }
+                ]
+            }
+        ],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Chrome'],
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: true
-  });
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
 };
