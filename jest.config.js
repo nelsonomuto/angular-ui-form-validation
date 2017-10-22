c7fd7b318f2286066b8b80418ab8c3aa53b9f17a
Nelson Omuto <nelsonomuto@gmail.com>
2017-10-21 20:49:58 -0400

    wip

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  setupFiles: ['<rootDir>/jest/requestAnimationFrame.js'],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: ['node_modules', 'app'],
+  moduleNameMapper: {
+    // Use proxy to mock CSS Modules. Lookups on the styles object will be returned as-is
+    // (e.g., styles.foobar === 'foobar')
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+
+    // Transform file imports into file names
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jest/fileTransformer.js',
+  },
+  testMatch: ['**/test/**/**spec.js']
+};
